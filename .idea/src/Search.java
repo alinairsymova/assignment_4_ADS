7daff89a61ab389fa132b1b386b273f049e0bcce
Alina <241516@astanait.edu.kz>
2025-05-21 20:58:27 +0500

    ваптпав

---
+++ .idea/src/Search.java
@@ -0,0 +1,13 @@
+package assignment_4_ADS;
+
+import java.util.List;
+
+public abstract class Search<V> {
+    protected WeightedGraph<V> graph;
+
+    public Search(WeightedGraph<V> graph) {
+        this.graph = graph;
+    }
+
+    public abstract List<Vertex<V>> findPath(V start, V end);
+}
